ca330f40513ebd4ef4344c7fff31d0478fd6f9a1
mflibby <mackenzieflibby@gmail.com>
2020-03-02 14:33:03 -0500

    further abstraction of integration function

--- integration.py
+++ integration.py
@@ -1,12 +1,16 @@
 def integrate(function,bounds,Δx=0.0001,area_func= lambda function, x, Δx : function(x)*Δx ):
-    import numpy as np
-    area = 0
-    x_list = np.arange(bounds[0],bounds[1],Δx)
+    """
+    Calculates the integral of an arbitrary function numerically using an arbitrary area function (the area function defaults to a basic
+    rectangular area, with no preference to circumscribing vs inscribing)
 
-    area = area_func(function,x,Δx)
-    #for i in x_list:
-    #    try:
-    #        area += area_func(function,i,Δx)
-    #    except:
-    #        print('something went wrong...')
-    return area
+    INPUTS:
+    function  - like y = lambda x : x**2
+    bounds    - 1D array with 2 elements, like [-5,100]
+    Δx        - step size (i.e. width of areas). must be scalar
+    area_func - could be a function of any kind, but must be prepared to deal with array multiplication like Δx*(np.arange(0,10,1)**2)
+
+    RETURNS:
+    area - area of the integral
+    """
+    import numpy as np
+    return sum(area_func(function, np.arange(bounds[0],bounds[1],Δx), Δx))
