e97685c63770d22bc883fb726cdbd6542ed045d2
mflibby <mackenzieflibby@gmail.com>
2020-03-05 17:27:19 -0500

    primitive optimizeIntegral class added for testing

--- integration.py
+++ integration.py
@@ -29,3 +29,36 @@ def trap_integrate(function,bounds,Δx = 0.0001):
     area - area of the integral
     """
     return integrate(function,bounds,Δx,lambda function, x, Δx: (function(x)+function(x+Δx))*Δx*0.5)
+
+
+class optimizeIntegral():
+    """
+    Use dynamic step size to ensure numerical integration has minimal eroneous values due to x-axis area cross over.
+
+    INPUTS:
+    function  - like y = lambda x : x**2
+    bounds    - 1D array with 2 elements, like [-5,100]
+    Δx        - step size (i.e. width of areas). must be scalar
+    area_func - could be a function of any kind, but must be prepared to deal with array multiplication like Δx*(np.arange(0,10,1)**2)
+
+    RETURNS:
+    area - area of the integral
+    """
+    import numpy as np
+    def __init__(function, bounds, Δx , area_func = lambda function, x, Δx : function(x)*Δx):
+        self.func      = function
+        self.bounds    = bounds
+        self.step      = Δx
+        self.area_func = area_func
+
+
+    def roughOptimum():
+        """
+        Chops off cross-over values.
+        """
+        step_list = np.arange(self.bounds[0],self.bound[1],self.step)
+        areas = []
+        for i in step_list:
+            if abs(self.func(i)+self.func(i+self.step))>=abs(self.func(i)):
+                areas.append(self.area_func(self.func,i,self.step))
+        return sum(areas)
