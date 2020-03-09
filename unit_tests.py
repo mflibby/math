beb702229fa415708ce828bcef22e6f32f6c3159
mflibby <mackenzieflibby@gmail.com>
2020-03-09 12:42:33 -0400

    new unit tests

--- unit_tests.py
+++ unit_tests.py
@@ -8,5 +8,5 @@ import numpy as np
 y = lambda x: np.sin(x**2)/np.log(x)
 #i.trap_integrate(y,[0,2])
 
-integral = i.optimizeIntegral(y,[1.1,10.1],0.00001)
+integral = i.optimizeIntegral(y,[1.1,10.1],0.00001, lambda function, x, Δx : .5*(function(x)+function(x+Δx))*Δx)
 print('Dynamic Optimum: ', integral.dynamicOptimum(), '\nRough Optimum: ', integral.roughOptimum())
