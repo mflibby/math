34b7bbecd0142465706ff883933301263e205947
mflibby <mackenzieflibby@gmail.com>
2022-01-15 09:39:23 -0500

    mandle brot

--- hcrd_vis.py
+++ hcrd_vis.py
@@ -4,42 +4,55 @@ import numpy as np
 import cmath
 import time as t
 
-x = np.linspace(-1.5,2,1000)
+x = np.linspace(-1.5,1.5,1000)
 y = np.linspace(-1.5,1.5,1000)*1j
 
-start = t.time_ns()
-c = np.array([[i+j for i in x] for j in y])
-#print(c)
+# start = t.time_ns()
+class MandelBrot:
 
-znext = lambda z, c: z**2 + c
+    def __init__(self, xlims= (-1.5,1.5), ylims=(-1.5,1.5),resolution = 1000):
+        self.x = np.linspace(xlims[0],xlims[1],resolution)
+        self.y = np.linspace(ylims[0],ylims[1],resolution)*1j
 
-def perform_iter (func, seed,n = 200, delta = 10):
-    prev = 0 + 0j
-    changes = []
-    for i in range(n):
-        prev = func(prev, seed)
-        if np.isnan(abs(prev)):
-            return False
-    return not (abs(prev) > delta)
+        self.c = np.array([[i+j for i in x] for j in y])
+        # print(f"Complex field generated in {(t.time_ns() - start)/10**9}s...")
 
+        self.znext = lambda z, c: z**2 + c
 
-temp = []
-for row in c:
-    for item in row:
-        if abs(item)<1.5 and perform_iter(znext, item):
-            temp.append(item)
+    def perform_iter (self, seed, n = 10, delta = 1.5):
+        prev = 0 + 0j
+        for _ in range(n):
+            prev = self.znext(prev, seed)
+            if np.isnan(abs(prev)) or abs(prev) > delta:
+                return False
+        return not (abs(prev) > delta)
 
-#print(temp)
-x = [i.real for i in temp]
-y = [i.imag for i in temp]
-# print(y)
+    def run_animate (self, n_range, step = 1):
+        for n in range(n_range[0], n_range[1]+1, step):
+            self.run( n )
 
-print((t.time_ns() - start)/10**9 )
-fig,ax = plt.subplots(1,1,figsize = (14,14))
+    def run(self, n = 100 ):
+        temp = []
+        for row in self.c:
+            for item in row:
+                if self.perform_iter(item,n):
+                    temp.append(item)
 
-ax.scatter(x,y, s = .7, color = "#3A507D")
-ax.grid(False)
-ax.axis('off')
-plt.savefig("MandleBrot.png")
-#ax.set_ylim([-10,10])
-plt.show()
+        print(f"Mandel brot generated {n}...")
+        #print(temp)
+        x = [i.real for i in temp]
+        y = [i.imag for i in temp]
+        # print(y)
+
+        # print((t.time_ns() - start)/10**9 )
+        fig,ax = plt.subplots(1,1,figsize = (14,14))
+
+        ax.scatter(x,y, s = .1, color = "#3A507D")
+        ax.grid(False)
+        ax.axis('off')
+        plt.savefig(f"brots\MandleBrot_{n}.png")
+        # plt.show()
+
+brot = MandelBrot()
+
+brot.run_animate([5,20])
